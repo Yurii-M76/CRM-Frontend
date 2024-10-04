540bb099ff82df89224d70828924fa714442d5bd
Yurii M <ispoyar@yandex.ru>
2024-10-04 14:57:09 +0300

    refactoring api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-import { getCookie, setCookie } from "./cookie";
+import { getCookie } from "./cookie";
 import { TUser } from "./types";
 
 const HOST = import.meta.env.VITE_API_URL;
@@ -8,67 +8,17 @@ const URL = `${HOST}:${PORT}`;
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 
-type TServerResponse<T> = {
+type TServerResponse = {
   success: boolean;
-} & T;
-
-type TRefreshResponse = TServerResponse<{
   refreshToken: string;
   accessToken: string;
-}>;
-
-export const refreshToken = (): Promise<TRefreshResponse> =>
-  fetch(`${URL}/auth/token`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({
-      token: localStorage.getItem("refreshToken"),
-    }),
-  })
-    .then((res) => checkResponse<TRefreshResponse>(res))
-    .then((refreshData) => {
-      if (!refreshData.success) {
-        return Promise.reject(refreshData);
-      }
-      localStorage.setItem("refreshToken", refreshData.refreshToken);
-      setCookie("accessToken", refreshData.accessToken);
-      return refreshData;
-    });
-
-export const fetchWithRefresh = async <T>(
-  url: RequestInfo,
-  options: RequestInit
-) => {
-  try {
-    const res = await fetch(url, options);
-    return await checkResponse<T>(res);
-  } catch (err) {
-    if ((err as { message: string }).message === "jwt expired") {
-      const refreshData = await refreshToken();
-      if (options.headers) {
-        (options.headers as { [key: string]: string }).authorization =
-          refreshData.accessToken;
-      }
-      const res = await fetch(url, options);
-      return await checkResponse<T>(res);
-    } else {
-      return Promise.reject(err);
-    }
-  }
+  user: string;
 };
 
 export type TLoginData = {
   email: string;
   password: string;
 };
-
-type TAuthResponse = TServerResponse<{
-  refreshToken: string;
-  accessToken: string;
-  user: TUser;
-}>;
 
 export const loginUserApi = (data: TLoginData) =>
   fetch(`${URL}/auth/login`, {
@@ -78,22 +28,26 @@ export const loginUserApi = (data: TLoginData) =>
     },
     body: JSON.stringify(data),
   })
-    .then((res) => checkResponse<TAuthResponse>(res))
+    .then((res) => checkResponse<TServerResponse>(res))
     .then((data) => {
-      if (data) return data;
+      if (data?.success) return data;
       return Promise.reject(data);
     });
-
-type TUserResponse = TServerResponse<{ user: TUser }>;
-
-export const getUserApi = () =>
-  fetchWithRefresh<TUserResponse>(`${URL}/user`, {
-    headers: {
-      authorization: getCookie("accessToken"),
-    } as HeadersInit,
-  });
 
 export const logoutApi = () =>
   fetch(`${URL}/auth/logout`, {
     method: "GET",
   }).then((res) => res.status);
+
+export const getMeApi = () =>
+  fetch(`${URL}/user/`, {
+    method: "GET",
+    headers: {
+      authorization: getCookie("accessToken"),
+    } as HeadersInit,
+  })
+    .then((res) => checkResponse<TUser>(res))
+    .then((data) => {
+      if (data) return data;
+      return Promise.reject(data);
+    });
