cb008e50b6f9f751b64d25b109321e3e175b3392
Yurii M <ispoyar@yandex.ru>
2024-10-21 16:46:56 +0300

    fix api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,4 @@
-import { getCookie } from "./cookie";
-import { TUser } from "./types";
+import { deleteCookie, getCookie } from "./cookie";
 
 const HOST = import.meta.env.VITE_API_URL;
 const PORT = import.meta.env.VITE_API_PORT;
@@ -8,11 +7,17 @@ const URL = `${HOST}:${PORT}`;
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 
-type TServerResponse = {
-  // success: boolean;
-  // refreshToken: string;
+type TRefreshToken = {
+  token: string;
+  exp: string;
+  userId: string;
+  userAgent: string;
+};
+
+type TAuthResponse = {
+  refreshToken: TRefreshToken;
   accessToken: string;
-  // user: string;
+  success: boolean;
 };
 
 export type TLoginData = {
@@ -28,26 +33,34 @@ export const loginUserApi = (data: TLoginData) =>
     },
     body: JSON.stringify(data),
   })
-    .then((res) => checkResponse<TServerResponse>(res))
+    .then((res) => checkResponse<TAuthResponse>(res))
     .then((data) => {
-      if (data) return data;
+      if (data.success) return data;
       return Promise.reject(data);
     });
 
 export const logoutApi = () =>
   fetch(`${URL}/api/auth/logout`, {
     method: "GET",
-  }).then((res) => res.status);
+  }).then((res) => {
+    deleteCookie("accessToken");
+    deleteCookie("refreshToken");
+    return res.status;
+  });
 
-export const getMeApi = () =>
-  fetch(`${URL}/api/user/`, {
+export const refreshTokens = () =>
+  fetch(`${URL}/api/auth/refresh-tokens`, {
     method: "GET",
     headers: {
+      "Content-Type": "application/json;charset=utf-8",
       authorization: getCookie("accessToken"),
     } as HeadersInit,
+    credentials: "include", // отправка cookies с клиента
   })
-    .then((res) => checkResponse<TUser>(res))
-    .then((data) => {
-      if (data) return data;
-      return Promise.reject(data);
+    .then((res) => checkResponse<TAuthResponse>(res))
+    .then((refreshData) => {
+      if (refreshData.success) {
+        return refreshData;
+      }
+      return Promise.reject(refreshData);
     });
