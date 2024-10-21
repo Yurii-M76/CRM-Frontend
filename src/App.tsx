b9175b9791109f379e13e9486bec6f31b1bb66a5
Yurii M <ispoyar@yandex.ru>
2024-10-21 16:47:08 +0300

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Router } from "./router";
 import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
-import { useDispatch } from "./services/store";
 import { useEffect } from "react";
+import { useDispatch } from "./services/store";
+import { Router } from "./router";
+import { theme } from "./theme";
 import { checkUserAuth } from "./services/user/action";
-
 import "@mantine/core/styles.css";
 
 function App() {
