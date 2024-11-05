1d8028cd7578ab79a0c4dce5e0d2a8071fdfd007
Yurii M <ispoyar@yandex.ru>
2024-11-05 16:25:59 +0300

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { MantineProvider } from "@mantine/core";
-import { useDispatch } from "@services/store";
+import { useDispatch, useSelector } from "@services/store";
 import { checkUserAuth } from "@services/user/action";
 import { useEffect } from "react";
 import { Router } from "./router";
 import { theme } from "./theme";
+import { Login } from "./pages";
+import { getIsAuthChecked } from "./services/user/reducer";
 import "@mantine/core/styles.css";
 
 function App() {
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector(getIsAuthChecked);
   useEffect(() => {
     dispatch(checkUserAuth());
   }, [dispatch]);
 
+
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      {!isAuthenticated && <Login />}
+      {isAuthenticated && <Router />}
     </MantineProvider>
   );
 }
