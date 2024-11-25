f621fd5fa294933cde3d6f6fa6a0775e316f9420
Yurii M <ispoyar@yandex.ru>
2024-11-25 12:06:04 +0300

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@services': path.resolve(__dirname, './src/services'),
       '@utils': path.resolve(__dirname, './src/utils'),
+      '@types': path.resolve(__dirname, './src/types'),
     },
   },
   plugins: [react()],
