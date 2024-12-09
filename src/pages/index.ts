ab76794dc19d10b9a47f4112d2c29b00dfe0f7a3
Yurii M <ispoyar@yandex.ru>
2024-12-09 16:38:09 +0300

    the admin page has been added

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,7 @@ export { DashboardPage } from "./dashboard/dashboard.page";
 export { LoginPage } from "./login/login.page";
 export { MailingPage } from "./mailing/mailing.page";
 export { NotFoundPage } from "./not-found/not-found.page";
+export { PersonsPage } from "./persons/persons.page"
 export { ProjectsPage } from "./projects/projects.page";
 export { SettingsPage } from "./settings/settings.page";
-export { PersonsPage } from "./persons/persons.page"
+export { AdminPage } from "./admin/admin.page";
