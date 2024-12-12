8307c2058345fe4f5685d458339eefffc2e314a9
Yurii M <ispoyar@yandex.ru>
2024-12-12 17:40:49 +0300

    fix fn format date to string

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -4,10 +4,10 @@
  * @param param ("asc" | "desc") форматирует дату в строку вида "DD.MM.YYYY" или YYYY-MM-DD соответственно
  */
 
-export const formatDate = (
+export const formatDateToString = (
   date: Date | undefined,
   param: "asc" | "desc"
-): string | undefined => {
+): string => {
   if (date) {
     const day = date.getDate().toString().padStart(2, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
@@ -15,5 +15,5 @@ export const formatDate = (
     if (param === "asc") return `${day}.${month}.${year}`;
     if (param === "desc") return `${year}-${month}-${day}`;
   }
-  return undefined;
+  return "";
 };
