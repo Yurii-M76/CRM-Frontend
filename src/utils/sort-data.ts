26628fbea7fa2786b8a08ffc5b68e271659fbe2f
Yurii M <ispoyar@yandex.ru>
2024-12-16 18:36:53 +0300

    add processing of null values

--- src/utils/sort-data.ts
+++ src/utils/sort-data.ts
@@ -5,20 +5,30 @@
  * @param sortOrder направление сортировки
  * @returns результат сортирвки, если данные являются строкой / числом (в противном случае 0)
  */
-export const sortData = <T>(data: T[], sortBy: keyof T, sortOrder: "asc" | "desc") => {
+
+export const sortData = <T>(
+  data: T[],
+  sortBy: keyof T,
+  sortOrder: "asc" | "desc"
+) => {
   if (!data.length) return data;
   return data.sort((a, b) => {
     const aValue = a[sortBy];
     const bValue = b[sortBy];
+
+    // Обработка случая, когда aValue или bValue равны null
+    if (aValue === null && bValue === null) return 0; // оба null, равны
+    if (aValue === null) return sortOrder === "asc" ? -1 : 1; // null считается меньшим
+    if (bValue === null) return sortOrder === "asc" ? 1 : -1; // null считается меньшим
+
     if (typeof aValue === "string" && typeof bValue === "string") {
       return sortOrder === "asc"
         ? aValue.localeCompare(bValue)
         : bValue.localeCompare(aValue);
     } else if (typeof aValue === "number" && typeof bValue === "number") {
-      return sortOrder === "asc"
-        ? aValue - bValue
-        : bValue - aValue;
+      return sortOrder === "asc" ? aValue - bValue : bValue - aValue;
     } else {
+      console.error("Sorting error");
       return 0;
     }
   });
