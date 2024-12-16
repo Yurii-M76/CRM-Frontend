b9ffaa90c795e1defd8f3e19254dcab941f89783
Yurii M <ispoyar@yandex.ru>
2024-12-16 18:37:14 +0300

    fix farmat date fn

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -7,7 +7,7 @@
 export const formatDateToString = (
   date: Date | undefined,
   param: "asc" | "desc"
-): string => {
+): string | undefined => {
   if (date) {
     const day = date.getDate().toString().padStart(2, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
@@ -15,5 +15,5 @@ export const formatDateToString = (
     if (param === "asc") return `${day}.${month}.${year}`;
     if (param === "desc") return `${year}-${month}-${day}`;
   }
-  return "";
+  return undefined;
 };
