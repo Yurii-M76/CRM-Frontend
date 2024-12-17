09c29b800bf36079dd43bb132c9f163883f42f5a
Yurii M <ispoyar@yandex.ru>
2024-12-17 10:09:07 +0300

    added sorting of arrays and objects

--- src/utils/sort-data.ts
+++ src/utils/sort-data.ts
@@ -17,9 +17,9 @@ export const sortData = <T>(
     const bValue = b[sortBy];
 
     // Обработка случая, когда aValue или bValue равны null
-    if (aValue === null && bValue === null) return 0; // оба null, равны
-    if (aValue === null) return sortOrder === "asc" ? -1 : 1; // null считается меньшим
-    if (bValue === null) return sortOrder === "asc" ? 1 : -1; // null считается меньшим
+    if (aValue === null && bValue === null) return 0;
+    if (aValue === null) return sortOrder === "asc" ? -1 : 1;
+    if (bValue === null) return sortOrder === "asc" ? 1 : -1;
 
     if (typeof aValue === "string" && typeof bValue === "string") {
       return sortOrder === "asc"
@@ -27,6 +27,14 @@ export const sortData = <T>(
         : bValue.localeCompare(aValue);
     } else if (typeof aValue === "number" && typeof bValue === "number") {
       return sortOrder === "asc" ? aValue - bValue : bValue - aValue;
+    } else if (Array.isArray(aValue) && Array.isArray(bValue)) {
+      return sortOrder === "asc"
+        ? aValue.length - bValue.length
+        : bValue.length - aValue.length;
+    } else if (typeof aValue === "object" && typeof bValue === "object") {
+      return sortOrder === "asc"
+        ? JSON.stringify(aValue).localeCompare(JSON.stringify(bValue))
+        : JSON.stringify(bValue).localeCompare(JSON.stringify(aValue));
     } else {
       console.error("Sorting error");
       return 0;
