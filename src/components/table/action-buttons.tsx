4e3f58ff8c030beeb95931dae42d6276ce0e65e8
Yurii M <ispoyar@yandex.ru>
2025-01-06 16:53:05 +0300

    delete tooltip

--- src/components/table/action-buttons.tsx
+++ src/components/table/action-buttons.tsx
@@ -14,28 +14,24 @@ export const ActionButtons: FC<TActionButtons> = ({
 }) => {
   return (
     <div className={classes.actionButtons}>
-      <Tooltip label="Редактировать">
-        <ActionIcon
-          aria-label="Редактировать"
-          variant="light"
-          radius="xl"
-          color="orange"
-          onClick={handleClickFromEdit}
-        >
-          <Icons.IconEdit className={classes.iconActionButtons} />
-        </ActionIcon>
-      </Tooltip>
-      <Tooltip label="Удалить">
-        <ActionIcon
-          aria-label="Удалить"
-          variant="light"
-          radius="xl"
-          color="red"
-          onClick={handleClickFromDelete}
-        >
-          <Icons.IconTrash className={classes.iconActionButtons} />
-        </ActionIcon>
-      </Tooltip>
+      <ActionIcon
+        aria-label="Редактировать"
+        variant="light"
+        radius="xl"
+        color="orange"
+        onClick={handleClickFromEdit}
+      >
+        <Icons.IconEdit className={classes.iconActionButtons} />
+      </ActionIcon>
+      <ActionIcon
+        aria-label="Удалить"
+        variant="light"
+        radius="xl"
+        color="red"
+        onClick={handleClickFromDelete}
+      >
+        <Icons.IconTrash className={classes.iconActionButtons} />
+      </ActionIcon>
     </div>
   );
 };
