ad4ecf3f9adcafa40b344f6b81d80c2ae447669f
Yurii M <ispoyar@yandex.ru>
2025-01-08 18:16:12 +0300

    added checking admin rights

--- src/router.tsx
+++ src/router.tsx
@@ -8,52 +8,58 @@ import {
   ProjectsPage,
   SettingsPage,
 } from "./pages";
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/persons",
-          element: <PersonsPage />,
-        },
-        {
-          path: "/projects",
-          element: <ProjectsPage />,
-        },
-        {
-          path: "/mailing",
-          element: <MailingPage />,
-        },
-        {
-          path: "/settings",
-          element: <SettingsPage />,
-        },
-        {
-          path: "/admin-page",
-          element: <AdminPage />,
-        },
-      ],
-    },
-    {
-      path: "*",
-      element: <NotFoundPage />,
-    },
-  ],
-  {
-    future: {
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_partialHydration: true,
-      v7_normalizeFormMethod: true,
-      v7_skipActionErrorRevalidation: true,
-    },
-  }
-);
+import { getMeData } from "@/services/user/reducer";
+import { useSelector } from "./services/store";
 
 export function Router() {
+  const user = useSelector(getMeData);
+  const isAdmin = user?.roles.includes("ADMIN");
+  const adminPage = isAdmin ? <AdminPage /> : "Ошибка 403. Доступ запрещен";
+
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "/persons",
+            element: <PersonsPage />,
+          },
+          {
+            path: "/projects",
+            element: <ProjectsPage />,
+          },
+          {
+            path: "/mailing",
+            element: <MailingPage />,
+          },
+          {
+            path: "/settings",
+            element: <SettingsPage />,
+          },
+          {
+            path: "/admin-page",
+            element: adminPage,
+          },
+        ],
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+    {
+      future: {
+        v7_relativeSplatPath: true,
+        v7_fetcherPersist: true,
+        v7_partialHydration: true,
+        v7_normalizeFormMethod: true,
+        v7_skipActionErrorRevalidation: true,
+      },
+    }
+  );
+
   return (
     <RouterProvider
       router={router}
