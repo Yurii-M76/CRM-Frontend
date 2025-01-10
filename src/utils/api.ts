9a4fb1e4d2d90fa450f6d61f5d027230ae59aa3c
Yurii M <ispoyar@yandex.ru>
2025-01-10 16:52:22 +0300

    fixed refreshTokens

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -29,7 +29,7 @@ export const refreshTokens = async (): Promise<TAuthResponse> => {
       method: "GET",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
-        authorization: getCookie("accessToken"),
+        authorization: getCookie("accessToken")?.replace("Bearer ", ""),
       } as HeadersInit,
       credentials: "include",
     });
