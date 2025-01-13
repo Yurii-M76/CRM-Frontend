4128f2380db76a498aaeabb07931f2265797dbff
Yurii M <ispoyar@yandex.ru>
2025-01-13 20:35:57 +0300

    added actions users

---
+++ src/services/users/actions.ts
@@ -0,0 +1,35 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  createDataFromApi,
+  deleteDataFromApi,
+  getAllDataFromApi,
+  getOneDataFromApi,
+  updateDataFromApi,
+} from "@/utils";
+import { TUser } from "@/types";
+
+export const createUser = createAsyncThunk(
+  "users/create",
+  async (data: TUser) => await createDataFromApi<TUser>("users", data)
+);
+
+export const findAllUsers = createAsyncThunk(
+  "users/findAll",
+  async () => await getAllDataFromApi<TUser[]>("users")
+);
+
+export const findOneUser = createAsyncThunk(
+  "users/findOne",
+  async (id: string) => await getOneDataFromApi<TUser>("users", id)
+);
+
+export const updateUser = createAsyncThunk(
+  "users/update",
+  async ({ id, data }: { id: string; data: TUser }) =>
+    await updateDataFromApi<TUser>("users", id, data)
+);
+
+export const deleteUser = createAsyncThunk(
+  "users/delete",
+  async (id: string) => await deleteDataFromApi<{ id: string }>("users", id)
+);
