b0239b673c1ce418d55d47c6673736b168b55de7
Yurii M <ispoyar@yandex.ru>
2025-01-21 16:00:59 +0300

    added projects actions

--- src/services/project/action.ts
+++ src/services/project/action.ts
@@ -1,8 +1,31 @@
 import { TProject } from "@/types";
-import { getAllDataFromApi } from "@/utils";
+import {
+  createDataFromApi,
+  deleteDataFromApi,
+  getAllDataFromApi,
+  updateDataFromApi,
+} from "@/utils";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const path = "projects";
 
 export const findAllProjects = createAsyncThunk(
   "projects/findAll",
-  async () => await getAllDataFromApi<TProject[]>("projects")
+  async () => await getAllDataFromApi<TProject[]>(path)
+);
+
+export const createProject = createAsyncThunk(
+  "project/create",
+  async (data: TProject) => await createDataFromApi<TProject>(path, data)
+);
+
+export const updateProject = createAsyncThunk(
+  "project/update",
+  async ({ id, data }: { id: string; data: Partial<TProject> }) =>
+    await updateDataFromApi<TProject>(path, id, data)
+);
+
+export const deleteProject = createAsyncThunk(
+  "project/delete",
+  async (id: string) => await deleteDataFromApi<{ id: string }>(path, id)
 );
