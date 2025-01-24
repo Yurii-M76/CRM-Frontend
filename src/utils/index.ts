17afb761b97587068c7fbf72aeebf961ebe09e1a
Yurii M <ispoyar@yandex.ru>
2025-01-24 13:52:30 +0300

    fixed path

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 export * from './api';
 export * from './cookie';
-export * from './format-date';
+export * from './format-date-to-string';
 export * from './filter-data';
 export * from './pagination';
 export * from './sort-data';
