3a47a11efb0db797bdd079b1b9b6310aab08d2ff
Yurii M <ispoyar@yandex.ru>
2025-01-29 11:59:05 +0300

    added read & reset errors mesage & alert message & data to update set in the form

--- src/components/forms/users/form-save-user.tsx
+++ src/components/forms/users/form-save-user.tsx
@@ -1,25 +1,38 @@
-import { ActionIcon, Select, Switch, TextInput, Tooltip } from "@mantine/core";
+import {
+  ActionIcon,
+  Anchor,
+  Select,
+  Switch,
+  TextInput,
+  Tooltip,
+  Center,
+  Alert,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { FC } from "react";
-import { UserRole } from "@/types";
+import { FC, useEffect } from "react";
+import { TUser, UserRole } from "@/types";
 import { useDispatch, useSelector } from "@/services/store";
 import { createUser } from "@/services/users/actions";
 import { ButtonsDefaultFromForm } from "@/components/forms/elements/buttons";
-import { getStatusUsers } from "@/services/users/reducer";
+import {
+  getErrors,
+  getStatusUsers,
+  resetErrors,
+} from "@/services/users/reducer";
 import exceptions from "@/constants/exceptions";
 import * as Icons from "@assets/icons";
 import classes from "../forms.module.css";
 
 type TFormSaveUser = {
   onClose?: () => void;
+  updData?: TUser;
 };
 
 type TInitialValues = {
   name: string;
   email: string;
   password: string;
-  confirmPassword: string;
-  role: UserRole | "";
+  role: string | "";
   isBlocked: boolean;
 };
 
@@ -43,20 +56,21 @@ const generatePassword = (): string => {
   return passwordParts.join("-");
 };
 
-const FormSaveUser: FC<TFormSaveUser> = ({ onClose }) => {
+const FormSaveUser: FC<TFormSaveUser> = ({ onClose, updData }) => {
   const dispatch = useDispatch();
   const status = useSelector(getStatusUsers);
-  const isLoadingCreate = status.create.loading;
-  // const isLoadingUpdate = status.update.loading;
+  const isLoading = status.create.loading || status.update.loading;
+  const errorMessage = useSelector(getErrors);
 
   const initialValues: TInitialValues = {
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    role: "",
-    isBlocked: false,
+    name: updData?.name || "",
+    email: updData?.email || "",
+    password: (updData && "**********") || "",
+    role: updData?.role || "",
+    isBlocked: updData?.isBlocked || false,
   };
+
+  const isUpdData = updData ? true : false;
 
   const roles = Object.entries(UserRole).map(([key, label]) => ({
     value: key,
@@ -64,7 +78,7 @@ const FormSaveUser: FC<TFormSaveUser> = ({ onClose }) => {
   }));
 
   const form = useForm({
-    mode: "uncontrolled",
+    mode: "controlled",
     initialValues: initialValues,
     validate: {
       name: (value) =>
@@ -92,6 +106,10 @@ const FormSaveUser: FC<TFormSaveUser> = ({ onClose }) => {
     dispatch(createUser(form.getValues()));
   };
 
+  useEffect(() => {
+    dispatch(resetErrors());
+  }, [dispatch, form.values]);
+
   return (
     <form
       onSubmit={form.onSubmit(handleSubmit)}
@@ -110,24 +128,29 @@ const FormSaveUser: FC<TFormSaveUser> = ({ onClose }) => {
           label="Email"
           key={form.key("email")}
           {...form.getInputProps("email")}
+          disabled={isUpdData}
         />
+        {isUpdData && <Anchor size="sm">Изменить email</Anchor>}
         <TextInput
           id="password"
           label="Пароль"
           key={form.key("password")}
           {...form.getInputProps("password")}
+          disabled={isUpdData}
           rightSection={
-            <Tooltip label="Сгенерировать пароль">
+            <Tooltip label={"Сгенерировать"}>
               <ActionIcon
                 variant="subtle"
                 color="indigo"
                 onClick={handleGeneratePassword}
+                disabled={isUpdData}
               >
-                <Icons.IconbrandSupabase strokeWidth={1.5} />
+                <Icons.IconBrandSupabase strokeWidth={1.5} />
               </ActionIcon>
             </Tooltip>
           }
         />
+        {isUpdData && <Anchor size="sm">Изменить пароль</Anchor>}
         <Select
           id="role"
           label="Роль"
@@ -135,17 +158,31 @@ const FormSaveUser: FC<TFormSaveUser> = ({ onClose }) => {
           {...form.getInputProps("role")}
           data={roles}
         ></Select>
-        <Switch
-          id="isBlocked"
-          label="Заблокирован"
-          key={form.key("isBlocked")}
-          {...form.getInputProps("isBlocked")}
-          defaultChecked={false}
-          color="red"
-          className={classes.inputHeight}
-        />
+        {isUpdData && (
+          <Switch
+            id="isBlocked"
+            label="Заблокировать"
+            key={form.key("isBlocked")}
+            {...form.getInputProps("isBlocked")}
+            defaultChecked={false}
+            color="red"
+            className={classes.inputHeight}
+          />
+        )}
       </div>
-      <ButtonsDefaultFromForm onClose={onClose} loading={isLoadingCreate} />
+      {errorMessage && (
+        <Center>
+          <Alert
+            variant="light"
+            color="red"
+            title="Ошибка"
+            icon={<Icons.IconExclamationCircle strokeWidth={2} />}
+          >
+            {errorMessage}
+          </Alert>
+        </Center>
+      )}
+      <ButtonsDefaultFromForm onClose={onClose} loading={isLoading} />
     </form>
   );
 };
