0dff9e21a4d0bce62a5d0e518d9de202ffa7dc29
Yurii M <ispoyar@yandex.ru>
2025-02-18 14:36:06 +0300

    modified export to excel function

--- src/utils/export-to-excel.ts
+++ src/utils/export-to-excel.ts
@@ -1,24 +1,92 @@
-import * as XLSX from "xlsx";
+import ExcelJS, { Column } from "exceljs";
 
-export const exportToExcel = (
-  sheetName: string,
-  header: string[],
-  data?: unknown[]
+/**
+ * 
+ * @param data - unknow[]
+ * @param columns - { header: string, key: string, width: number }[]
+ * @param nestedLists - { key: string, label: string }[]
+ * @param textFormat - boolean
+ * @param filename - string
+ */
+
+export const exportToExcel = async <T>(
+  data: T[],
+  columns: Partial<Column>[],
+  nestedLists: { key: string; label: string }[],
+  textFormat: boolean,
+  filename: string
 ) => {
-  // создаём новую книгу и лист
-  const workbook = XLSX.utils.book_new();
-  const worksheet = XLSX.utils.json_to_sheet(data || []);
-  // const headerRow = XLSX.utils.aoa_to_sheet([header], { cellStyles: true });
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet(filename);
+  const dataLength = data.length;
+  worksheet.columns = columns;
 
-  // добавляем заголовки в лист
-  XLSX.utils.sheet_add_json(worksheet, data || [], {
-    skipHeader: true,
-    origin: "A2",
+  if (dataLength) {
+    data.forEach((row) => {
+      worksheet.addRow(row);
+    });
+  }
+
+  const headerStyle = { bold: true, color: { argb: "000000" } };
+  worksheet.getRow(1).font = headerStyle;
+
+  worksheet.addTable({
+    name: `${filename}_table`,
+    ref: `A1:${String.fromCharCode(65 + columns.length - 1)}${dataLength + 1}`,
+    headerRow: true,
+    totalsRow: false,
+    style: {
+      theme: "TableStyleLight15",
+      showRowStripes: false,
+    },
+    columns: columns.map((column) => ({ name: column.header as keyof Column })),
+    rows: !dataLength
+      ? [[]]
+      : data.map((item) =>
+          columns.map((column) => {
+            const value = column.key as keyof T;
+            if (Array.isArray(item[value])) {
+              if (nestedLists.length) {
+                const enumMatch = nestedLists.find(
+                  (listItem) => column.key === listItem.key
+                );
+                return enumMatch
+                  ? item[value]
+                      .map((subItem) => subItem[enumMatch.label])
+                      .join("; ")
+                  : item[value].join("; ");
+              } else {
+                return "";
+              }
+            }
+            return item[value];
+          })
+        ),
   });
-  XLSX.utils.sheet_add_aoa(worksheet, [header], { origin: "A1" });
 
-  // добавляем лист в книгу
-  XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
-  // записываем в файл и запускаем загрузку
-  XLSX.writeFile(workbook, `${sheetName}.xlsx`);
+  if (textFormat) {
+    columns.forEach((_, index) => {
+      worksheet.getColumn(index + 1).numFmt = "@"; // текстовый формат ячеек
+    });
+  }
+
+  columns.forEach((_, index) => {
+    worksheet.getColumn(index + 1).alignment = {
+      vertical: "top",
+      horizontal: "left",
+      wrapText: true,
+    }; // выравнивание и перенос текста в ячейке
+  });
+
+  const buffer = await workbook.xlsx.writeBuffer();
+  const blob = new Blob([buffer], { type: "application/octet-stream" });
+  const url = window.URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `${filename}.xlsx`;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  window.URL.revokeObjectURL(url);
 };
